0fc98d4f09752b06ff99d2ba1c243c756ee80f3d
August <hello@aughost.codes>
2023-03-05 20:33:51 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const completedArea = document.querySelector('.completed-area');
+const rollingArea = document.querySelector('.rolling-area');
+const ship = document.getElementById('ship');
+const captain = document.getElementById('captain');
+const crew = document.getElementById('crew');
+
+let rollArray = [];
+let diceRemaining = 5;
+let turnsRemaining = 3;
+let roundsRemaining;
+let shipTrue = false;
+let captainTrue = false;
+let crewTrue = false;
+
+const rollDie = () => {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    // console.log(roll);
+    return roll;
+}
+
+const rollAllFiveDice = () => {
+    for (let i = 0; i < 5; i++) {
+        rollArray.push(rollDie());
+    }
+    console.log(rollArray);
+}
+
+const checkForShip = () => {
+    if (rollArray.includes(6)) {
+        ship.style.backgroundImage = "url('./img/dice6.png')";
+        diceRemaining--;
+        console.log(`Dice remaining: ${diceRemaining}`);
+        shipTrue = true;
+    }
+}
+
+const checkForCaptain = () => {
+    if ((rollArray.includes(6) && rollArray.includes(5)) || (shipTrue && rollArray.includes(5))) {
+        captain.style.backgroundImage = "url('./img/dice5.png')";
+        diceRemaining--;
+        console.log(`Dice remaining: ${diceRemaining}`);
+        captainTrue = true;
+    }
+}
+
+const checkForCrew = () => {
+    if ((rollArray.includes(6) && rollArray.includes(5) && rollArray.includes(4)) || (captainTrue && rollArray.includes(4))) {
+        crew.style.backgroundImage = "url('./img/dice4.png')";
+        diceRemaining--;
+        console.log(`Dice remaining: ${diceRemaining}`);
+        crewTrue = true;
+    }
+}
+
+const checkForSCC = () => {
+    checkForShip();
+    checkForCaptain();
+    checkForCrew();
+    turnsRemaining--;
+    if (turnsRemaining === 0) {
+        // tally up total of last two dice and put total in cargo hold
+        console.log('GAME IS OVER');
+    }
+}
+
+const rollRemainingDice = () => {
+    if (shipTrue) {
+        console.log('You found the ship');
+        for (let i = 0; i < 4; i++) {
+            console.log(rollDie());
+        }
+    }
+}
+
+function updateRollArea() {
+    if (rollDie() === 6) {
+        ship.style.backgroundImage = "url('./img/dice6.png')";
+        diceRemaining--;
+        console.log(diceRemaining);
+        shipTrue = true;
+    }
+}
